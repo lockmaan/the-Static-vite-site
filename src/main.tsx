a8ca61c57d1d4fc342834d27159d389ca1737817
unknown <lokma-men@outlook.fr>
2025-09-07 17:32:37 +0100

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import './style.scss'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+
+    <!--flex  -->
+<header class="card">Lokmene araar</header>
+<div class="swat">
+    
+    <div class="one">
+        <div id="one" class="card">1</div>
+        <div id= "two" class="card">2</div>
+    </div>
+    <div class= "two">
+        <div id="three" class="card">3</div>
+        <div id="four" class="card">4</div>
+    </div>
+    <div class= "three">
+        <div id="five" class="card" >5</div>
+        <div id="six" class="card">6</div>
+    </div>
+
+<div>
+</div>
+
+
+
+`
+
+
+
