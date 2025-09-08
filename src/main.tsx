a75b2820e69292e402c887025edaea42fd3708ac
loklok <lokma-men@outlook.fr>
2025-09-08 18:46:13 +0100

    begginning to add text to the website

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import './phone.scss'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
     <!--flex sasa -->
-<header class="card">Lokmene araar</header>
+    <header class="card">
+    <nav class="name">Lokmene <br><b>araar</b>
+    </nav> 
+    <div></div>
+
+
+</header>
 <div class="swat">
     
     <div class="one">
@@ -12,7 +18,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
         <div id= "two" class="card">2</div>
     </div>
     <div class= "two">
-        <div id="three" class="card">3</div>
+        <div id="three" class="card"><img src="../public/loklok.jpg" alt="" /></div>
         <div id="four" class="card">4</div>
     </div>
     <div class= "three">
