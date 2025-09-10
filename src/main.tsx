5babdf4dfb28bfd89c3b9fd29968b01d5c8127fd
loklok <lokma-men@outlook.fr>
2025-09-10 13:04:11 +0100

    finished adding the html necessary

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,61 @@
-import './style.scss'
-import './phone.scss'
+import './styles/style.scss'
+import './styles/phone.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
     <!--flex sasa -->
     <header class="card">
-    <nav class="name">Lokmene <br><b>araar</b>
-    </nav> 
-    <div></div>
+    <nav class="name">Lokmene <br><b>araar</b></nav>
+    <a href="" className="project">Projects</a>
 
 
 </header>
 <div class="swat">
     
     <div class="one">
-        <div id="one" class="card">1</div>
-        <div id= "two" class="card">2</div>
+        <div id="one" class="card">
+            <img src="/square-root.svg" alt="" />
+            <p>
+            Programmer Redefining <br />Web-dev with<br />ease of access
+            </p>
+            
+            </div>
+        <div id= "two" class="card">
+            <p>I'll tell you about myself. 
+            I am a web developer with a passion for creating beautiful and functional websites. with math background.
+            An ex student in algeirs USTHB university.
+            I have a strong understanding of HTML, CSS, and JavaScript, and I am always looking to learn new technologies and techniques.
+            </p>
+        
+        </div>
     </div>
     <div class= "two">
-        <div id="three" class="card"><img src="../public/loklok.jpg" alt="" /></div>
-        <div id="four" class="card">4</div>
+        <div id="three" class="card"><img src="/loklok.jpg" alt="" /></div>
+        <div id="four" class="card">
+        <div>
+            <p>Have some questions ?</p><div></div>
+            </div>
+            <a href="lokmamen@gmail.com">Contact me</a>
+        </div>
     </div>
     <div class= "three">
-        <div id="five" class="card" >5</div>
-        <div id="six" class="card">6</div>
+        <div id="five" class="card" >
+            <div>
+            <p>Musea</p>
+            <div></div>
+            </div>
+            <img src="/computer-program-coding-screen_53876-138060.jpg" alt="" />
+            <div></div>
+            <div></div>
+        
+        </div>
+        <div id="six" class="card">
+        <div className="icon"></div>
+        <div className="icon"></div>
+        <div className="icon"></div>
+        
+        
+        </div>
     </div>
 
 </div>
